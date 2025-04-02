9b5ea3b59b5c65f58df1812a6c2da84b87d1f589
kalutwarro1 <kalutwarro@gmail.com>
2025-04-02 11:41:56 -0300

    adds table structure and fetch

--- front/src/script.js
+++ front/src/script.js
@@ -10,20 +10,43 @@ document.addEventListener("DOMContentLoaded", function () {
         loader.setAttribute("role", "status");
         loader.innerHTML = '<span class="visually-hidden">Loading...</span>';
         
-        // Insert loader into the DOM
+        // Sumar loader
         ejecutarBtn.parentNode.appendChild(loader);
-        
-        // After timeout, replace loader with "Reporte generado!"
-        setTimeout(() => {
-            loader.remove();
-            const message = document.createElement("p");
-            message.className = "mt-1 text-success-emphasis fw-bold";
-            message.textContent = "📄 Reporte generado!";
-            ejecutarBtn.parentNode.appendChild(message);
-        }, 3000);
+
     });
 });
 
-function runPython() {
-    
-}
+function fetchCSVData() {
+    fetch("http://localhost:3434/generate")
+      .then((response) => response.json())
+      .then((data) => {
+        populateTable(data)})
+      .catch((error) => console.error("Error fetching data:", error));
+  }
+  
+  function populateTable(data) {
+    const tableBody = document.getElementById("table-body");
+    tableBody.innerHTML = "";
+  
+    data.forEach((row) => {
+      console.log("Row Data:", row); // debug
+  
+      const tr = document.createElement("tr");
+  
+      tr.innerHTML = `
+        <td>${row.empleado_id || "N/A"}</td>
+        <td>${row.fecha || "N/A"}</td>
+        <td>${row.dia_semana || "N/A"}</td>
+        <td>${row.asistencia || "N/A"}</td>
+        <td>${row.hora_entrada || "N/A"}</td>
+        <td>${row.hora_salida || "N/A"}</td>
+        <td>${row.tipo_ausencia || "N/A"}</td>
+        <td>${row.min_entrada || "N/A"}</td>
+        <td>${row.min_salida || "N/A"}</td>
+        <td>${row.anomalia || "N/A"}</td>
+      `;
+  
+      tableBody.appendChild(tr);
+    });
+  }
+  
