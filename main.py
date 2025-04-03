b70339d82ec67714503a748375d9dc26951da6f6
jorgedem <jorgedema@yahoo.com>
2025-04-02 21:31:14 -0300

    Update main.py
    Quita funciones irrelevantes para el análisis de los datos

--- main.py
+++ main.py
@@ -13,14 +13,8 @@ def main():
     # Procesar datos
     df_asistencia = procesar_datos.pre_procesar_datos("asistencia_empleados.csv")
 
-    # Entrenar el modelo Isolation Forest
-    modelo = procesar_datos.entrenar_isolation_forest(df_asistencia)
-
-    # Generar DataFrame final, sólo con los datos relevantes
+    # Generar DataFrame final, con los datos procesados
     df_final = procesar_datos.resumir_datos_asistencia(df_asistencia)
-
-    # Genera reporte de asistencias anómalas
-    procesar_datos.empleados_asistencia_anomala(df_final, "anomalias_de_asistencia.csv")
 
     # Detecta el porcentaje con peor cumplimiento
     procesar_datos.detectar_peores_empleados(df_final, 20, "empleados_mas_incumplidores.csv")
