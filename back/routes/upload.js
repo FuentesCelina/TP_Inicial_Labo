c6c97511b4f5bb798faa38cbb4ca9ab2c52f2655
FuentesCelina <fuentescelina01@gmail.com>
2025-04-10 15:13:26 -0300

    se toma info del back

--- back/routes/upload.js
+++ back/routes/upload.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const path = require('path');
 
 const router = express.Router();
+const { spawn } = require('child_process');
+
 
 // Configuración de multer (memoria, sin guardar archivos en disco)
 const storage = multer.diskStorage({
@@ -11,7 +13,7 @@ const storage = multer.diskStorage({
       cb(null, 'uploads/'); // Carpeta donde se guarda el archivo
     },
     filename: function (req, file, cb) {
-      const uniqueName = Date.now() + '-' + file.originalname;
+      const uniqueName = 'asistencia_empleados.csv';
       cb(null, uniqueName);
     }
   });
@@ -33,13 +35,31 @@ router.post('/upload-csv', upload.single('csvFile'), (req, res) => {
       return res.status(400).json({ message: 'Faltan datos en la solicitud' });
     }
 
+     // Primero se ejecuta esta función interna
     generateTxtFile(percentage, columns);
 
-    res.status(200).json({
-      message: 'Archivo y datos recibidos correctamente',
-      fileName: file.originalname,
-      percentage,
-      columns
+     // Ahora ejecutamos el script de Python
+     const pythonScriptPath = path.join(__dirname, "../src/main.py");
+     const pythonProcess = spawn('python', [pythonScriptPath]);
+ 
+     pythonProcess.stdout.on('data', (data) => {
+       console.log(`Salida de Python: ${data}`);
+     });
+ 
+     pythonProcess.stderr.on('data', (data) => {
+       console.error(`Error del script Python: ${data}`);
+     });
+ 
+     pythonProcess.on('close', (code) => {
+       console.log(`Python terminó con código ${code}`);
+ 
+       // Respondemos cuando todo haya terminado
+       res.status(200).json({
+         message: 'Archivo procesado y script ejecutado correctamente',
+         fileName: file.originalname,
+         percentage,
+         columns
+       });
     });
 
   } catch (error) {
@@ -50,7 +70,7 @@ router.post('/upload-csv', upload.single('csvFile'), (req, res) => {
 
 function generateTxtFile(percentage, columns){
      // Crear archivo datos.txt en ../
-     const outputPath = path.join(__dirname, '../../datos.txt');
+     const outputPath = path.join(__dirname, '../uploads/datos.txt');
      const content = `${percentage}\n${columns.join('\n')}`;
      fs.writeFileSync(outputPath, content);
      console.log('Archivo de salida generado en:', outputPath);
